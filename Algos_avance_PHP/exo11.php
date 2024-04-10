34bf723b376e6bb55ab7d2a78269a9ee72945c28
Hippolyte-Gander <165796324+Hippolyte-Gander@users.noreply.github.com>
2024-04-10 13:41:01 +0200

    Add files via upload

--- Algos_avance_PHP/exo11.php
+++ Algos_avance_PHP/exo11.php
@@ -7,29 +7,15 @@ formaterDateFr("2018-02-23");</p>
 
 <?php
 
-function formatDateFr($dateString) {
-
-    $joursfr = ["dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi"];
-    $moisfr = [1=>"janvier", 2=>"février", 3=>"mars", 4=>"avril", 5=>"mai", 6=>"juin", 7=>"juillet", 8=>"août", 9=>"septembre", 10=>"octobre", 11=>"novembre", 12=>"décembre"];
-    
-    $date = DateTime::createFromFormat('Y-m-d', $dateString);
-    
-    if ($date === false) {
-        echo "Format de date invalide";
-    }
-
-    $jourSemaine = date_format($date, "w");
-    $jour = date_format($date, "j");
-    $mois = intval(date_format($date, "n"));
-    $annee = date_format($date, "Y");
-
-    $resultat = $joursfr[$jourSemaine] ." ". $jour ." ". $moisfr[$mois] ." ". $annee;
+function formatDateFr($date) {
+    $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::FULL, IntlDateFormatter::NONE);
+        
+    $resultat = $formatter->format($date);
 
     return $resultat;
 }
 
-$dateString = "2018-02-23";
 
-echo formatDateFr($dateString);
-
+$date = new DateTime('2018-02-23');
+echo formatDateFr($date);
 ?>
